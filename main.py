1f823f17b0629a611fd35ed54568f00264f6441d
isa806444 <isa806444@gmail.com>
2026-01-26 11:50:30 -0500

    Move scan route before app.run

--- main.py
+++ main.py
@@ -19,11 +19,6 @@ def ping():
         "message": "pong"
     })
 
-if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
-
 @app.route("/scan")
 def scan():
     return {
@@ -33,3 +28,8 @@ def scan():
             {"ticker": "SGMT", "price": 1.12, "signal": "Breakout"}
         ]
     }
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
